e2244d6fb608d2484c94ab2ba0ed6ce6e1417fe6
Luc <luc.bayo@gmail.com>
2019-05-11 19:14:33 +0200

    Add nice landing page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,22 +4,33 @@ export default class Home extends Component {
   render() {
     return (
       <div className="home-page">
-        <div className="starter-btn">
-          <div className="btn join-now">
-            <button
-              onClick={() => this.props.history.push('/session')}
-              className="round-btn"
-            >
-              Join Chat Now
-            </button>
+        <div className="content">
+          <div className="brand">
+            <h1 className="brand-name">WeForHealth</h1>
           </div>
-          <div className="btn join-now">
-            <button
-              onClick={() => this.props.history.push('/suggestions')}
-              className="round-btn"
-            >
-              Leave Suggestion
-            </button>
+          <div className="starter-btn">
+            <div className="btn join-now">
+              <button
+                onClick={() => this.props.history.push('/session')}
+                className="round-btn"
+              >
+                Join Chat Now
+              </button>
+            </div>
+            <div className="btn join-now">
+              <button
+                onClick={() => this.props.history.push('/suggestions')}
+                className="round-btn"
+              >
+                Leave Suggestion
+              </button>
+            </div>
+          </div>
+          <div className="quote">
+            <p>
+              I understood myself only after I destroyed myself. And only in the
+              process of fixing myself, did I know who I really was.{' '}
+            </p>
           </div>
         </div>
       </div>
